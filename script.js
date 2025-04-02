bb8fea567876e184f8a59e5cb257a8a2972ad62e
JhonYuusha <158830813+JhonYuusha@users.noreply.github.com>
2025-04-02 12:58:04 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const fadeInElements = document.querySelectorAll('.fade-in');
+
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.style.opacity = 1;
+                entry.target.style.transform = "translateY(0)";
+            }
+        });
+    }, { threshold: 0.2 });
+
+    fadeInElements.forEach(el => observer.observe(el));
+});
